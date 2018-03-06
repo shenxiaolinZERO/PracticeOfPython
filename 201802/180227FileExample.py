7c828ca4c7d02a69c50eb201c9b18974b4e6257e
shenxiaolinZERO <2311513185@qq.com>
2018-03-06 22:22:38 +0800

    20180227da

--- 201802/180227FileExample.py
+++ 201802/180227FileExample.py
@@ -74,15 +74,85 @@ def drawMain():
 
 # drawMain()
 
+
+
 ######----------------------文件实例二
 #多文件读写的例子
 #编写程序将电话簿TeleAddressBook.txt和电子邮件EmailAddressBook.txt合并为一个完整的AddressBook.txt
+#（利用字符串和列表将两个通讯录合并为一个文本）
 
-#打开文件、读取文件：
-ftele1=open('EmailAddressBook.txt','rb')
-ftele2=open('TeleAddressBook.txt','rb')
+#完整代码如下：
+def AddressMain():
+    # 打开文件、读取文件：
+    ftele1 = open('180227data2-TeleAddressBook.txt', 'rb')
+    ftele2 = open('180227data2-EmailAddressBook.txt', 'rb')
 
-ftele1.readlines()#跳过第一行
-ftele2.readlines()
-lines1=ftele1.readlines()
-lines2=ftele2.readlines()
+    print("《电话簿》如下：")
+    for line in ftele1:
+        print(str(line.decode('gbk')))
+    print("\n《邮件簿》如下：")
+    for line in ftele2:
+        print(str(line.decode('gbk')))
+    ftele1.readlines()  # 跳过第一行
+    ftele2.readlines()
+    lines1 = ftele1.readlines()
+    lines2 = ftele2.readlines()
+
+    # 建立空列表用于存储姓名、电话、Email：
+    list1_name = []
+    list1_tele = []
+    list2_name = []
+    list2_email = []
+
+    print(lines1)
+    # 获取TeleAddressBook中的信息：
+    for line in lines1:  # 获取第一个文本中的姓名和电话信息
+
+        elements = line.split()
+        list1_name.append(str(elements[0].decode('gbk'))) #将文本读出来的bytes转换为str类型
+        list1_tele.append(str(elements[1].decode('gbk')))
+
+    # 获取EmailAddressBook中的信息：
+    for line in lines2:  # 获取第二个文本中的姓名和邮件信息
+        elements = line.split()
+        list2_name.append(str(elements[0].decode('gbk')))
+        list2_email.append(str(elements[1].decode('gbk')))
+
+    # 开始合并处理：
+    # 1.生成新的数据：
+    lines = []
+    lines.append('姓名\t    电话    \t 邮箱 \n')
+    # 2.按索引方式遍历姓名列表1
+    for i in range(len(list1_name)):
+        s = ''
+        if list1_name[i] in list2_name:
+            j = list2_name.index(list1_name[i])  # 找到姓名列表1对应列表2中的姓名索引位置
+            s = '\t'.join([list1_name[i], list1_tele[i], list2_email[j]])
+            s += '\n'
+        else:
+            s = '\t'.join([list1_name[i], list1_tele[i], str('   -----   ')])
+            s += '\n'
+        lines.append(s)
+
+    # 3.处理姓名列表2剩余的姓名
+    for i in range(len(list2_name)):
+        s = ''
+        if list2_name[i] not in list1_name:
+            s = '\t'.join([list2_name[i], str('   -----    '), list2_email[i]])
+            s += '\n'
+        lines.append(s)
+
+    # 将新生成的合并数据写入新的文件中：
+    print("\n新生成的数据为：")
+    print(lines)
+    ftele3 = open('AddressBook.txt', 'w')
+    ftele3.writelines(lines)
+
+    # 关闭文件
+    ftele3.close()
+    ftele2.close()
+    ftele1.close()
+
+    print("\nThe AddressBooks are merged !")
+if __name__ == '__main__':
+    AddressMain()
