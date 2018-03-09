d1278811f21d7b9facd204a4d6e703bea27acbdc
shenxiaolinZERO <2311513185@qq.com>
2018-03-09 20:27:47 +0800

    20180229

--- 201802/180229FileandDicExercise.py
+++ 201802/180229FileandDicExercise.py
@@ -4,10 +4,12 @@ __author__ = 'Administrator'
 
 #【Exercises】
 #1. 理解文本和二进制打开方式的区别
-fopen1=open("180228data2-test.txt","r")
+fopen1=open("180229data1-testCN.txt","r")
+# lines=fopen1.readlines()
+# print(lines)
 for line in fopen1:
-     print(line.encode("ASCII"))
-fopen2=open("180228data2-test.txt","rb")
+     print(line)  #中国是个伟大国家！
+fopen2=open("180229data1-testCN.txt","rb")
 print(fopen2)
 #2. 文件处理，打开文件并逐行处理
     # fo=open(fname,"r")
