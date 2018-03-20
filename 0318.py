cd30b792a5c395d36f9482cbb2a6fcdf59b0a68c
shenxiaolinZERO <2311513185@qq.com>
2018-03-20 21:28:44 +0800

    countTime2

--- 0318.py
+++ 0318.py
@@ -2,11 +2,23 @@
 # encoding: utf-8
 __author__ = 'Administrator'
 
-session_item_dic={ 0:[[1,2,3],[4,5,6]] }
-dic = {}
-for i, j in session_item_dic.items():
-    item_list = j
-    # item_list.extend(j[1])
-    dic[i] = item_list
-print(dic)
+# session_item_dic={ 0:[[1,2,3],[4,5,6]] }
+# dic = {}
+# for i, j in session_item_dic.items():
+#     item_list = j
+#     # item_list.extend(j[1])
+#     dic[i] = item_list
+# print(dic)
+#
 
+from pandas import Series,DataFrame
+
+# series=Series(["Kangkang","Michale","Jane","Maria"])
+# print(series)
+#
+# print(series.values)
+# print(series.index)
+
+data={"name":["Kangkang","Michale","Jane","Maria"],"age":["18","19","20","21"]}
+dataFrame=DataFrame(data,columns=["name","age","score"])
+print(dataFrame)
--- 180320CountDays.py
+++ 180320CountDays.py
@@ -1,9 +1,14 @@
 # !/usr/bin/env python
 # encoding: utf-8
 __author__ = 'Administrator'
+
+###--Python实现天数倒计时计算
+#tips：在datetime模块里有一个计算时间差的 timedelta。
+#让两个datetime对象相减就得到timedelta
+
 from datetime import datetime
 #构造一个将来的时间
-future = datetime.strptime('2019-02-01 8:13:01','%Y-%m-%d %H:%M:%S')
+future = datetime.strptime('2019-02-01 08:00:00','%Y-%m-%d %H:%M:%S')
 #当前时间
 now = datetime.now()
 #求时间差
@@ -11,5 +16,7 @@ delta = future - now
 hour = delta.seconds/60/60
 minute = (delta.seconds - hour*60*60)/60
 seconds = delta.seconds - hour*60*60 - minute*60
-print("距离2019年02月01日还剩下：",delta.days)
-, hour, minute, seconds
+print_now=now.strftime('%Y-%m-%d %H:%M:%S')
+print("今天是：",print_now)
+print("距离 2019-02-01 \"work\" 还剩下：%d天"%delta.days)
+print(delta.days,hour, minute, seconds)
