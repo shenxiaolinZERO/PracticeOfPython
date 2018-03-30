f187837bc4545e1a4ddeec4d8607ced16c5f66cb
shenxiaolinZERO <2311513185@qq.com>
2018-03-30 21:16:06 +0800

    190315OSplatformProgram

---
+++ 201803/190315OSplatformProgram.py
@@ -0,0 +1,42 @@
+# !/usr/bin/env python
+# encoding: utf-8
+__author__ = 'Administrator'
+
+#OS平台编程
+
+# OS平台编程的需求
+#  目录文件的操作
+# 对系统目录、文件的操作方法
+#  程序定时执行
+#  可执行程序的转换
+# python程序向可执行程序的转换
+
+# 目录文件的操作 os库
+# python安装后自带的函数库，处理操作系统相关功能
+# os.getcwd() 获得当前工作目录
+# os.listdir(path) 返回指定目录下的所有文件和目录名
+# os.remove() 删除一个文件
+# os.removedirs(path) 删除多个目录
+# os.chdir(path) 更改当前目录到指定目录
+# os.mkdir(path) 新建一个目录
+# os.rmdir(name) 删除一个目录
+# os.rename(src, dst) 更改文件名
+# os.path 处理操作系统目录的一个子库
+# Os.path.isfile() 检验路径是否是一个文件
+# Os.path.isdir()
+# Os.path.exists() 判断路径是否存在
+# Os.path.split() 返回一个路径的目录名和文件名
+# os.path.splitext() 分离扩展名
+# Os.path.dirname 获得路径名
+# Os.path.basename() 获得文件名
+# Os.path.getsize() 获得文件大小
+# Os.path.join(path, name) 返回绝对路径
+# os.walk(path)用于遍历一个目录，返回一个三元组
+# root, dirs, files = os.walk(path)
+# 其中，root是字符串，dirs和files是列表类型，表示root中的所有目录和所有文件
+#......
+#......
+
+#......
+#......
+
