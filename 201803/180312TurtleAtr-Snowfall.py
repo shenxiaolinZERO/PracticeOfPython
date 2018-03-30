968f18c3fbfb04985dc249cd9326409055fe8202
shenxiaolinZERO <2311513185@qq.com>
2018-03-30 20:59:40 +0800

    180312TurtleAtr-Snowfall

---
+++ 201803/180312TurtleAtr-Snowfall.py
@@ -0,0 +1,63 @@
+# !/usr/bin/env python
+# encoding: utf-8
+__author__ = 'Administrator'
+
+#使用turtle库绘制雪景
+
+#随机因素：
+#雪花位置、雪花颜色、雪花大小、花瓣数目、地面灰色长短、地面灰色位置
+
+from turtle import *
+from random import *
+
+
+def ground():
+    hideturtle()
+    speed(100)
+    for i in range(400):
+        pensize(randint(5, 10))
+        x = randint(-400, 350)
+        y = randint(-280, -1)
+        r = -y / 280
+        g = -y / 280
+        b = -y / 280
+        pencolor((r, g, b))
+        penup()
+        goto(x, y)
+        pendown()
+        forward(randint(40, 100))
+
+
+def snow():
+    hideturtle()
+    pensize(2)
+    speed(100)
+    for i in range(100):
+        r = random()
+        g = random()
+        b = random()
+        pencolor(r, g, b)
+        penup()
+        setx(randint(-350, 350))
+        sety(randint(1, 270))
+        pendown()
+        dens = randint(8, 12)
+        snowsize = randint(10, 14)
+        for j in range(dens):
+            forward(snowsize)
+            backward(snowsize)
+            right(360 / dens)
+
+
+def main():
+    setup(800, 600, 0, 0)
+    tracer(False)
+    bgcolor("black")
+    snow()
+    ground()
+    tracer(True)
+    mainloop()
+
+
+if __name__ == "__main__":
+    main()
