f5f5411fa9fd767cd6b3fd88dcb883ec723a072e
shenxiaolinZERO <2311513185@qq.com>
2018-03-30 21:19:11 +0800

    180316OfficeProgram

---
+++ 201803/180316OfficeProgram.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+# encoding: utf-8
+__author__ = 'Administrator'
+
+#Office编程
+# Office编程的需求
+#  Word、Excel、Powerpoint等工具的编程处理
+#  使用Python程序协助处理上述文件类型
+#  用程序创建一个word文件，可以批量修改word文件
+#  用程序创建一个excel文件，批量修改或者对其中内容进
+# 行计算
+#  用程序创建一个powerpoint文件
+
+
+# 介绍一组实用的office文件操作函数库
+# xlwt 生成excel表单 pip 安装
+# Xlrd 读入并处理excel表单 pip 安装
+# Python-docx 创建并更新word文件 pip安装
+# lxml wheel安装
+# Python-pptx 创建并更新powerpoint文件 pip安装
