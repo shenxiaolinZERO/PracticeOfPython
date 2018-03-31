15762772619be4a346018d2430f12645ef1473be
shenxiaolinZERO <2311513185@qq.com>
2018-03-31 12:13:51 +0800

    180320-K-means

--- 201803/180320-K-Means.py
+++ 201803/180320-K-Means.py
@@ -34,8 +34,110 @@ __author__ = 'Administrator'
 #    调用kmeans聚类函数，得到聚类结果
 #    将聚类结果以图的形式展示出来。
 
+
+#  子函数定义
+#     Initialize center函数通过使用numpy库的zeros函数和random.uniform函数，
+#      随机选取了k个数据做聚类中心， 并将结果存放在Numpy的Array对象centers中
+#     Dist2Centers这个函数用来计算一个数据点到所有聚类中心的距离，将其存放在dis2cents中返回
+#     kmeans函数.
+#        这部分代码完成了kmeans算法中为数据点决定所属类别以及迭代更新类中心点的主要功能。
+#        注意numpy库的返回最小值索引的argmin函数以及计算平均值的mean函数的使用方法
+#     showcluster函数中，利用matplotlib库的plot函数将不同类别数据以不同颜色展现出来
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#子函数：Initialize center函数通过使用numpy库的zeros函数和random.uniform函数，
+#      随机选取了k个数据做聚类中心， 并将结果存放在Numpy的Array对象centers中
+def initCenters(dataSet,k):
+    numSamples,dim=dataSet.shape
+    centers=np.zeros((k,dim))
+    for i in range(k):
+        index=int(np.random.uniform(0,numSamples)) # random get k centers
+        centers[i,:]=dataSet[index,:]
+    print(centers)
+    return centers
+
+#子函数：Dist2Centers这个函数用来计算一个数据点到所有聚类中心的距离，将其存放在dis2cents中返回
+def Dist2Centers(sample,centers):
+    k=centers.shape[0]
+    dis2cents=np.zeros(k)
+    for i in range(k):
+        dis2cents[i]=np.sqrt(np.sum(np.power(sample-centers[i,:],2)))
+    return dis2cents
+
+#子函数：kmeans函数.
+#        这部分代码完成了kmeans算法中为数据点决定所属类别以及迭代更新类中心点的主要功能。
+#        注意numpy库的返回最小值索引的argmin函数以及计算平均值的mean函数的使用方法
+def kmeans(dataSet,k,iterNum):
+    numSamples=dataSet.shape[0]
+    iterCount=0
+
+    #clusterAssignment stores which cluster this sample belongs to
+    clusterAssignment=np.zeros(numSamples)
+    clusterChanged=True
+
+    ##step 1: initialize centers
+    centers=initCenters(dataSet,k)
+    while clusterChanged and iterCount <iterNum:
+        clusterChanged=False
+        iterCount=iterCount+1
+
+        # for each sample
+        for i in range(numSamples):
+            dis2cent=Dist2Centers(dataSet[i,:],centers)
+            minIndex=np.argmin(dis2cent) #返回最小值索引的argmin函数
+
+            ## step 3: update its belonged cluster
+            if clusterAssignment[i] !=minIndex:
+                clusterChanged=True
+                clusterAssignment[i]=minIndex
+
+        ## step 4: update centers
+        for j in range(k):
+            pointsInCluster=dataSet[np.nonzero(clusterAssignment[:]==j)[0]]
+            centers[j,:]=np.mean(pointsInCluster,axis=0) #计算平均值的mean函数
+    print("Congratulations ! Cluster Achieved !")
+    return centers,clusterAssignment
+
+#子函数：showcluster函数中，利用matplotlib库的plot函数将不同类别数据以不同颜色展现出来
+def showCluster(dataSet,k,centers,clusterAssignment):
+    numSamples,dim=dataSet.shape
+    mark=['or','ob','og','om']
+    #draw all samples
+    for i in range(numSamples):
+        markIndex=int(clusterAssignment[i])
+        plt.plot(dataSet[i,0],dataSet[i,1],mark[markIndex])
+
+    mark=['Dr','Db','Dg','Dm']
+    #draw the centroids 图心，几何中心
+    for i in range(k):
+        plt.plot(centers[i,0],centers[i,1],mark[i],markersize=17)
+    plt.show()
+
+
 def main():
     ## step 1: load dataset
     print("step 1: loading dataset...")
     dataSet=[]
     dataSetFile=open("180320-testSet.txt")
+    for line in dataSetFile:
+        lineArr=line.strip().split('\t')
+        dataSet.append([float(lineArr[0]),float(lineArr[1])])
+
+    ## step 2: clustering...
+    print("step 2: clustering...")
+    dataSet=np.mat(dataSet)
+
+    k=4
+    centers_result,clusterAssignment_result=kmeans(dataSet,k, 100)
+
+    ##step 3: show the result
+    print("tep 3: show the result...")
+    showCluster(dataSet,k,centers_result,clusterAssignment_result)
+main()
+
+# 实验结果：
+#  运行程序,
+#  下面依次是，将数据，聚为两类，三类，四类的程序结果图。也可以通过调整迭代次数，观察生成簇的变化
