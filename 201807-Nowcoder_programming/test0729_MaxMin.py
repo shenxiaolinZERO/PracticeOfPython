fbc8710c6f99c89a6ad84160afb8faa877d952d8
shenxiaolinZERO <2311513185@qq.com>
2018-07-29 10:50:18 +0800

    nowcoder

--- 201807-Nowcoder_programming/test0729_MaxMin.py
+++ 201807-Nowcoder_programming/test0729_MaxMin.py
@@ -5,4 +5,19 @@ __author__ = 'Administrator'
 # 20180729
 
 def MaxAndMin():
-    n = input()
+    n = int(input())
+    list = input().strip(" ")
+    max = 0
+    min = 0
+    for i in range(n):
+        max = int(list[i])
+        if int(list[i+1]) > max:
+            max = int(list[i+1])
+        min = int(list[i])
+        if int(list[i+1]) < min:
+            min = int(list[i+1])
+    print(max,min)
+
+
+if __name__ == '__main__':
+    MaxAndMin()
