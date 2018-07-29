a6dcc9e157473d495b33d79c4f18d9c5dbea8871
shenxiaolinZERO <2311513185@qq.com>
2018-07-29 10:41:00 +0800

    nowcoder-

--- 201807-Nowcoder_programming/test0727.py
+++ 201807-Nowcoder_programming/test0727.py
@@ -6,7 +6,6 @@ __author__ = 'Administrator'
 def xj(num):
     return sum(int(i) for i in str(num) if i.isdigit())
 
-
 if __name__ == '__main__':
     num = input('请输入一个整数: ')
     print('{} 每位数相加之和是: {}'.format(num, xj(num)))
