d9a943a6016bb2aaafff0b0f7fdd5ac43701aa4c
shenxiaolinZERO <2311513185@qq.com>
2018-07-29 12:42:31 +0800

    nowcoder-bugbugbugaaaaa......After all, I made it..

--- 201807-Nowcoder_programming/test0729_MaxMin.py
+++ 201807-Nowcoder_programming/test0729_MaxMin.py
@@ -7,22 +7,15 @@ __author__ = 'Administrator'
 def MaxAndMin():
     n = int(input())
     listStr = input().split()
-    print(listStr)
-    print(len(listStr))
-    # list = [4,5,6,8,2,9]
-    list = []
     for i in range(len(listStr)):
-        list[i] = int(listStr[i])
-    print("list为：",list)
-    # print(list)
-    max = list[0]
-    min = list[0]
+        listStr[i] = int(listStr[i])
+    max = listStr[0]
+    min = listStr[0]
     for i in range(n-1):
-        if list[i+1] > max:
-            max = list[i+1]
-    # for i in range(len(list)-1):
-        if list[i+1] < min:
-            min = list[i+1]
+        if listStr[i+1] > max:
+            max = listStr[i+1]
+        if listStr[i+1] < min:
+            min = listStr[i+1]
     print(max, min)
 
 
