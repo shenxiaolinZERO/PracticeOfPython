6b6fed9c86bb88dcb53903773069a4f9a116a6de
shenxiaolinZERO <2311513185@qq.com>
2018-07-30 12:59:15 +0800

    nowcoder-bugbugbugaaaaa......After all, I made it..

--- 201807-Nowcoder_programming/test0729_MaxMin.py
+++ 201807-Nowcoder_programming/test0729_MaxMin.py
@@ -18,6 +18,22 @@ def MaxAndMin():
             min = listStr[i+1]
     print(max, min)
 
+# 20180730 Triangle's three edge length
+def Triangle():
+    listStr = input().split()
+    for i in range(len(listStr)):
+        listStr = int(listStr[i])
+    max = listStr[0]
+    sum = 0
+    for i in range(len(listStr)-1):
+        if listStr[i+1] >max:
+            max = listStr[i+1]
+    for i in range(len(listStr)):
+        sum += listStr[i]
+    s = sum - 2*max
+    print(s)
+
 
 if __name__ == '__main__':
-    MaxAndMin()
+    # MaxAndMin()
+    Triangle()
