b293ee74864337112ad04de33ea740317a6f5ef7
shenxiaolinZERO <2311513185@qq.com>
2018-08-01 12:42:55 +0800

    nowcoder

--- 201807-Nowcoder_programming/test0727.py
+++ 201807-Nowcoder_programming/test0727.py
@@ -9,3 +9,12 @@ def xj(num):
 if __name__ == '__main__':
     num = input('请输入一个整数: ')
     print('{} 每位数相加之和是: {}'.format(num, xj(num)))
+
+# 20180731 对于每个输入数据，计算其各位数字之和，以及其平方值的数字之和，输出在一行中，之间用一个空格分隔，但行末不要有空格。
+def DigitalRoots3():
+    inputData = int(input())
+    powData = inputData * inputData
+    result1= sum(int(i) for i in str(inputData) if i.isdigit())
+    result2= sum(int(i) for i in str(powData) if i.isdigit())
+    print(result1,result2)
+DigitalRoots3()
