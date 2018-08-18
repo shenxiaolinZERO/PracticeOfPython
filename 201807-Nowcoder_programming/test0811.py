2ca555ca809b09b50a80aa5e0440533ce41cf101
shenxiaolinZERO <2311513185@qq.com>
2018-08-18 11:52:04 +0800

    nowcoder abc-#only one solution

--- 201807-Nowcoder_programming/test0811.py
+++ 201807-Nowcoder_programming/test0811.py
@@ -39,6 +39,11 @@ def GreatestCommonDivisor():
 # 20180818 Sat
 # 设a、b、c均是0到9之间的数字，abc、bcc是两个三位数，
 # 且有：abc+bcc=532。求满足条件的所有a、b、c的值。
+def  ABC( ):
+    c=1
+    b=3-c
+    a=5-b
+    print(a,b,c)  #实则为3 2 1 只有一组解
 
 if __name__ == '__main__':
     # MinStamp()
