8a4456d03cdab189e9071914913f3f0c7e340819
shenxiaolinZERO <2311513185@qq.com>
2018-09-09 19:01:43 +0800

    nowcoder

--- Nowcoder_programming/201809.py
+++ Nowcoder_programming/201809.py
@@ -2,27 +2,29 @@
 # encoding: utf-8
 __author__ = 'ScarlettZero'
 
-# 20180902 Sun 排序输出
-def SortN():
-    n = int(input())
-    list = map(int, input().split())
-    output = sorted(list)
-    # print(output)
-    for i in output:
-        # print(output[i] ,end=' ')
-        print(str(output[i ])+' ', end='')
-SortN()
+# # 20180902 Sun 排序输出
+# def SortN():
+#     n = int(input())
+#     list = map(int, input().split())
+#     output = sorted(list)
+#     # print(output)
+#     for i in output:
+#         # print(output[i] ,end=' ')
+#         print(str(output[i  ', end='')
+# SortN()
 
 
 # 20180908 Sat
 
 # 20180909 Sun
-def calculateEvenandOdd(n):
+def calculateEvenandOdd():
     list = map(int,input().split())
     for i in list:
-        print(i)
+        # print(i)
+        while i != 0 and i !=1:
+            
     # if  n%2 ==0:
     #     out = n/2
     # else:
     #     out = n*3+1
-
+calculateEvenandOdd()
