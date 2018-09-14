4ae20fb6914da30cbabd5449820297ce132f577f
shenxiaolinZERO <2311513185@qq.com>
2018-09-14 12:22:37 +0800

    A+B

--- Nowcoder_programming/201809.py
+++ Nowcoder_programming/201809.py
@@ -114,4 +114,7 @@ __author__ = 'ScarlettZero'
 # [7]
 # 给定两个整数A和B，其表示形式是：从个位开始，每三位数用逗号","隔开。 现在请计算A+B的结果，并以正常形式输出。
 def CalAPlusB():
+    AStr,BStr = input().split()
+    AStrList = AStr.split(",")
+    BStrList = BStr.split(",")
     
