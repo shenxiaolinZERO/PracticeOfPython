211d3de5a7ce55313aca2b6f4b0f5f4ddb0745dd
shenxiaolinZERO <2311513185@qq.com>
2018-09-28 17:31:39 +0800

    ReadingPaperSkill

--- 20180928ReadingPaperSkill.py
+++ 20180928ReadingPaperSkill.py
@@ -4,12 +4,14 @@ __author__ = 'Administrator'
 
 # python re 库是关于正则表达式的一个库
 import re
+import regex
 old_file = 'C:/Users/hj/Desktop/text.txt'
 fopen = open(old_file,'r')
 w_str =""
 for line in fopen:
     if re.search('\n',line):
-        line = re.sub('\n',' ',line):
+        # line = re.sub('\n',' ',line):
+        line = regex.sub('\n', ' ', line):
         w_str +=line
     else:
         w_str +=line
