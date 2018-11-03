0fb6a36d08123017fef1878fd7cd1bc88280dd78
shenxiaolinZERO <2311513185@qq.com>
2018-11-03 12:53:08 +0800

    Str

--- Nowcoder_programming/201811.py
+++ Nowcoder_programming/201811.py
@@ -49,7 +49,10 @@ def SameScoreStu():
 
 # [3] 求一个矩阵每列最大的两个数
 # 20181103 Saturday
-#
+#  思路：
+# 1）把列变为行；
+# 2）提取每行最大并去掉；
+# 3）
 def MaxTwoNum():
     numList = [[] for i in range(5)]
     result = [[],[]]
