35b65ae64cb01eb121063c31f6eb2daf6127d220
shenxiaolinZERO <2311513185@qq.com>
2018-11-08 12:47:16 +0800

    Str

--- Nowcoder_programming/201811.py
+++ Nowcoder_programming/201811.py
@@ -140,8 +140,16 @@ def NotRelatedWith7Main():
 # 20181108 Thursday
 # 判断最简真分数的函数
 def SimplestTrueFraction(a,b):
-
+    while b:
+        a,b = b,a%b
+    return a
 def STFMain():
     n = int(input())
     nums = list(map(int,input().split()))
-    
+    count = 0
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if SimplestTrueFraction(nums[i],nums[j])==1:
+                count+=1
+    print(count)
+STFMain()
