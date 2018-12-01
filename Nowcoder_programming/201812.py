8ec5ef4462912737b4a025fecf9295e57f9d09a7
shenxiaolinZERO <2311513185@qq.com>
2018-12-01 11:40:29 +0800

    Str

--- Nowcoder_programming/201812.py
+++ Nowcoder_programming/201812.py
@@ -5,4 +5,7 @@ __author__ = 'ScarlettZero'
 # 2018-December
 
 # [1] 拦截导弹
-# 20181201
+# 20181201 Saterday
+def interceptTheMissiles():
+    n = int(input())
+    listNum = list(map(int,input().split()))
