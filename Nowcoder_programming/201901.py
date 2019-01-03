e27bbfe8bca9a55538f710f0d8aa1fa0e9c29564
shenxiaolinZERO <2311513185@qq.com>
2019-01-03 15:14:24 +0800

    Str

--- Nowcoder_programming/201901.py
+++ Nowcoder_programming/201901.py
@@ -6,6 +6,8 @@ __author__ = 'ScarlettZero'
 
 # [1] 20190103 Thursday
 # 字符串排序
+# 把字符串里的字母全都复制到另一个字符串里，然后对另一个只含字母字符串进行排序。输出时对原字符串逐个字符输出，
+# 若该字符不是字母则直接输出，若是字母则从另一个字符串里面拿一个字符输出，这样就可以了
 def LongStringSort():
     while True:
         try:
@@ -13,7 +15,7 @@ def LongStringSort():
             outString = ""
             for i in inString:
 
-            return 0
+
 
         except:
             break
