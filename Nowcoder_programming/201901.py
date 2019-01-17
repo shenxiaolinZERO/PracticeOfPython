2d43b5e0231392baa3788d23653156804270187f
shenxiaolinZERO <2311513185@qq.com>
2019-01-17 16:45:20 +0800

    Str

--- Nowcoder_programming/201901.py
+++ Nowcoder_programming/201901.py
@@ -187,13 +187,17 @@ def MillionaireProblem():
     millionaire = 0 # 百万富翁给出的钱以分为单位
     for i in range(30):
         millionaire +=2**i
+    # ------------------------------------------
     # 转换前：(题目是要求先输出百万富翁交出的钱，后输出陌生人交出的钱)
     print(millionaire,stranger)
+    # ------------------------------------------
     # 转换后：
     # 题目要求输出结果：富翁交出的钱以万元为单位，陌生人交出的钱以分为单位
     # 万元和分转换为 10^6
-    millionaire = int(millionaire/(10**6))
-    stranger = stranger*(10**6)
-    print(millionaire,stranger)
+    millionaire2 = int(millionaire/(10**6))
+    stranger2 = stranger*(10**6)
+    print(millionaire2,stranger2)
+    #-------------------------------------------
+    # 但是上面都AC
 
 MillionaireProblem()
